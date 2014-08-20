8e928b4e28560db8aa25fda5ffae91122b719cd1
davidtucb <david.thompson@berkeley.edu>
2014-08-20 14:31:26 -0700

    Second

--- challenges/01-Intro/B_data_and_variables.py
+++ challenges/01-Intro/B_data_and_variables.py
@@ -3,20 +3,20 @@
 # instructors
 
 ## what are our names?
-instructors = 'dav'
+instructors = 'dav' and 'Sean' and 'Dave'
 
 # types
 ## Now define some other types of variables
 
 ## some_int should be an integer
-
+some_int = 3
 ## some_float should be a float
-
+some_float = 3.5
 ## some_bool should be... what do you think?
-
+some_bool = True
 # redefining
 ## Here, I define a variable
-my_lucky_number = 7
+my_lucky_number = 9
 
 ## But Dav's lucky number is 9 (and he wrote the test)
 ## Redefine the value "correctly" below
